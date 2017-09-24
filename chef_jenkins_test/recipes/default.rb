0eec96d4e290370a6acba466df54a106743a210e
ganeshgiri1984 <ganeshgiri.1984@gmail.com>
2017-09-24 09:44:16 -0700

    lastest commit

---
+++ chef_jenkins_test/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: chef_jenkins_test
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+directory '/opt/test123/' do
+	action	:create
+end
